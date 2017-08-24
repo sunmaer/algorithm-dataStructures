f11168279799b2e839cf3cbf4b86338b9bc5d162
sunmaer <878131732@qq.com>
2017-08-24 22:56:47 +0800

    解决栈错误，运行成功

--- quickSort.c
+++ quickSort.c
@@ -5,24 +5,26 @@
 
 /* 排序算法 */
 void quickSort(int arr[], int low, int high) {
-	int i=low, j=high, x=arr[i];
-	while(i<j) {
-		while(i < j && arr[j] >= x) {
-			j--;
+	if(low < high) {
+		int i=low, j=high, x=arr[low];
+		while(i<j) {
+			while(i < j && arr[j] >= x) {
+				j--;
+			}
+			if(i < j) { 
+				arr[i++] = arr[j];
+			}
+			while(i < j && arr[i] <= x) {
+				i++;
+			}
+			if(i < j) {
+				arr[j--] = arr[i];
+			}	
 		}
-		if(i < j) { 
-			arr[i++] = arr[j];
-		}
-		while(i < j && arr[i] <= x) {
-			i++;
-		}
-		if(i < j) {
-			arr[j--] = arr[i];
-		}	
+		arr[i] = x;
+		quickSort(arr, low, i-1);
+		quickSort(arr, i+1, high);
 	}
-	arr[i] = x;
-	quickSort(arr, low, i-1);
-	quickSort(arr, i+1, high);
 }
 
 /* 打印数组 */
@@ -36,7 +38,7 @@ void show(int arr[], int length) {
 
 int main() {
 	int arr[] ={ 48, 62, 35, 77, 55, 14, 35, 98 };
-	int length = 8, i;
+	int length = 8;
 	printf("排序前数组：");
 	show(arr, length);
 	quickSort(arr, 0, length);
